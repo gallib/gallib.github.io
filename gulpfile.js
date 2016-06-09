af4f89d1d88302a69e6722304aabdcb7552a3ae1
Alain Pellaux <alain@gallib.net>
2016-06-09 16:47:10 +0200

    watch changes on markdown files

--- gulpfile.js
+++ gulpfile.js
@@ -97,7 +97,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch(['_scss/*.scss'], ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*', '_config.yml', '_config_dev.yml'], ['jekyll-rebuild']);
+    gulp.watch(['*.md', '*.html', '_layouts/*.html', '_includes/*.html', '_posts/*', '_config.yml', '_config_dev.yml'], ['jekyll-rebuild']);
 });
 
 /**

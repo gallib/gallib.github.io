53e9b086c11f583c92930d7ec0c198815b69813a
Alain Pellaux <alain@gallib.net>
2022-03-31 08:39:52 +0200

    Better favicon support

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
         <Html lang="fr" className="h-full bg-myrtle-500">
             <Head>
                 <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
                     rel="stylesheet"

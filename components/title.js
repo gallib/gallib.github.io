ad93b5b2442e3aeab169682bf0b3e885832b46a4
Alain <alain@gallib.net>
2022-11-29 12:20:52 +0100

    Add Mastodon link

--- components/title.js
+++ components/title.js
@@ -10,33 +10,44 @@ export default function Title() {
             <div className="w-16 h-2 mt-5 bg-midred-500"></div>
             <div className="mt-10">
                 <Link href="https://github.com/gallib">
-                    <a>
+                    <a className="mr-10">
                         <img
                             className="inline-block"
                             src="/github.svg"
-                            alt="Mon compte GitHub"
+                            alt="Me retrouver sur GitHub"
                             width="50px"
                             height="50px"
                         />
                     </a>
                 </Link>
                 <Link href="https://twitter.com/gallib_net">
-                    <a className="mx-10">
+                    <a className="mr-10">
                         <img
                             className="inline-block"
                             src="/twitter.svg"
-                            alt="Mon compte Twitter"
+                            alt="Me retrouver sur Twitter"
                             width="50px"
                             height="50px"
                         />
                     </a>
                 </Link>
                 <Link href="https://ch.linkedin.com/in/alainpellaux">
-                    <a>
+                    <a className="mr-10">
                         <img
                             className="inline-block"
                             src="/linkedin.svg"
-                            alt="Mon compte LinkedIn"
+                            alt="Me retrouver sur LinkedIn"
+                            width="50px"
+                            height="50px"
+                        />
+                    </a>
+                </Link>
+                <Link href="https://tooting.ch/@iciAlain">
+                    <a rel="me">
+                        <img
+                            className="inline-block"
+                            src="/mastodon.svg"
+                            alt="Me retrouver sur Mastodon"
                             width="50px"
                             height="50px"
                         />
